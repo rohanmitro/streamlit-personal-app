e02cf74ba0649bb3e80a79aab9a8e0f30c74f42a
Rohan Mitra <mitrarm@gmail.com>
2023-08-22 20:23:26 +0530

    Add app.py

---
+++ app.py
@@ -0,0 +1,30 @@
+import streamlit as st
+
+# Title and Introduction
+st.title("Hi there! 👋")
+st.write("This is a single-page resource about me and my work experience.")
+
+# About Me
+st.header("About Me")
+st.write("I am a dedicated professional with a passion for technology and innovation.")
+st.write("I have 5 years experience in developing large scale data pipelines, data engineering and data extraction.")
+
+# Work Experience
+st.header("Work Experience")
+st.write("D. E. Shaw & Co. - Senior Software Engineer")
+
+st.write("LatentView Analytics - Data Analyst")
+
+# Additional Resources
+st.header("Additional Resources")
+st.subheader("LinkedIn Profile")
+st.write("[Link to My LinkedIn Profile](https://www.linkedin.com/in/rohan18/)")
+
+st.subheader("GitHub Repository")
+st.write("[Link to My GitHub Repository](https://github.com/rohanmitro)")
+
+# Contact Information
+st.header("Contact Information")
+st.write("Feel free to reach out to me at mitrarm@gmail.com")
+
+
